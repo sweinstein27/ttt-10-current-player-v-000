3ed8186bbe0d5749027abe639a0edbda00f93a6d
Scott Weinstein <sweinstein27@gmail.com>
2018-04-18 17:13:29 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,18 +1,27 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-turns = 0
-  board.each do |index|
-    if index == " X " || index == " O "
-      turns += 1
+def turn_count(turn)
+  #counter = 1
+  move_count = []
+  turn.each do |unit|
+    #counter = 0
+    if unit == "X" || unit == "O"
+      #puts "spot taken"
+      move_count.push(1)
+    #else
+      #puts "spot free"
+      #counter += 1
     end
   end
-  puts turns
+  move_count.length.to_i
+end
 
-
-# def current_player(board)
-#   if turn_count(board) % 2 == 0
-#     puts "X"
-#   else
-#     puts "O"
-#   end
-# end
+def current_player(board)
+  #if turn_count(board) %  == 0
+    num = turn_count(board)
+    if num % 2 == 0
+      return "X"
+    else
+      return "O"
+    #puts "divisible by 2"
+    end
+end
